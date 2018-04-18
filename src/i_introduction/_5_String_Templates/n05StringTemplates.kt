bfbd61ee5d648be1d1aa399cd4d4a01dba4c7f16
Nattasan Wang-Aphisit <xenogew@gmail.com>
2018-04-18 17:53:27 +0900

    solved the Task5

--- src/i_introduction/_5_String_Templates/n05StringTemplates.kt
+++ src/i_introduction/_5_String_Templates/n05StringTemplates.kt
@@ -23,7 +23,7 @@ fun getPattern() = """\d{2}\.\d{2}\.\d{4}"""
 
 fun example() = "13.06.1992".matches(getPattern().toRegex()) //true
 
-val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+const val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
 
 fun todoTask5(): Nothing = TODO(
     """
@@ -35,4 +35,4 @@ fun todoTask5(): Nothing = TODO(
     documentation = doc5(),
     references = { getPattern(); month })
 
-fun task5(): String = todoTask5()
+fun task5(): String = """\d{2} $month \d{4}"""
